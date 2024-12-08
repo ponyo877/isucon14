b40b2111919927f9d2f5282d2e8dad36e6a8777b
ponyo877 <keisuke877jp@yahoo.co.jp>
2024-12-08 11:55:27 +0900

    ownerGetChairsの改善_お試し

--- go/owner_handlers.go
+++ go/owner_handlers.go
@@ -195,27 +195,31 @@ func ownerGetChairs(w http.ResponseWriter, r *http.Request) {
 	owner := ctx.Value("owner").(*Owner)
 
 	chairs := []chairWithDetail{}
-	if err := db.SelectContext(ctx, &chairs, `SELECT id,
-       owner_id,
-       name,
-       access_token,
-       model,
-       is_active,
-       created_at,
-       updated_at,
-       IFNULL(total_distance, 0) AS total_distance,
-       total_distance_updated_at
-FROM chairs
-       LEFT JOIN (SELECT chair_id,
-                          SUM(IFNULL(distance, 0)) AS total_distance,
-                          MAX(created_at)          AS total_distance_updated_at
-                   FROM (SELECT chair_id,
-                                created_at,
-                                ABS(latitude - LAG(latitude) OVER (PARTITION BY chair_id ORDER BY created_at)) +
-                                ABS(longitude - LAG(longitude) OVER (PARTITION BY chair_id ORDER BY created_at)) AS distance
-                         FROM chair_locations) tmp
-                   GROUP BY chair_id) distance_table ON distance_table.chair_id = chairs.id
-WHERE owner_id = ?
+	if err := db.SelectContext(ctx, &chairs, `SELECT c.id,
+	   c.owner_id,
+	   c.name,
+	   c.access_token,
+	   c.model,
+	   c.is_active,
+	   c.created_at,
+	   c.updated_at,
+	   COALESCE(dt.total_distance, 0) AS total_distance,
+	   dt.total_distance_updated_at
+FROM chairs c
+LEFT JOIN (
+	SELECT cl.chair_id,
+		   SUM(COALESCE(cl.distance, 0)) AS total_distance,
+		   MAX(cl.created_at) AS total_distance_updated_at
+	FROM (
+		SELECT chair_id,
+			   created_at,
+			   ABS(latitude - LAG(latitude) OVER (PARTITION BY chair_id ORDER BY created_at)) +
+			   ABS(longitude - LAG(longitude) OVER (PARTITION BY chair_id ORDER BY created_at)) AS distance
+		FROM chair_locations
+	) cl
+	GROUP BY cl.chair_id
+) dt ON dt.chair_id = c.id
+WHERE c.owner_id = ?
 `, owner.ID); err != nil {
 		writeError(w, http.StatusInternalServerError, err)
 		return
