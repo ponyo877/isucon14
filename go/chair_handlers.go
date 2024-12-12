95562409524ce2ca5e74ba41f1d1b34f3c31dfb5
ponyo877 <keisuke877jp@yahoo.co.jp>
2024-12-13 01:20:02 +0900

    通知間隔の変更 1000ms -> 500ms

--- go/chair_handlers.go
+++ go/chair_handlers.go
@@ -227,7 +227,7 @@ func chairGetNotification(w http.ResponseWriter, r *http.Request) {
 	if err := tx.GetContext(ctx, ride, `SELECT * FROM rides WHERE chair_id = ? ORDER BY updated_at DESC LIMIT 1`, chair.ID); err != nil {
 		if errors.Is(err, sql.ErrNoRows) {
 			writeJSON(w, http.StatusOK, &chairGetNotificationResponse{
-				RetryAfterMs: 1000,
+				RetryAfterMs: 500,
 			})
 			return
 		}
@@ -239,7 +239,7 @@ func chairGetNotification(w http.ResponseWriter, r *http.Request) {
 	// if err != nil {
 	// 	if errors.Is(err, sql.ErrNoRows) {
 	// 		writeJSON(w, http.StatusOK, &chairGetNotificationResponse{
-	// 			RetryAfterMs: 1000,
+	// 			RetryAfterMs: 500,
 	// 		})
 	// 		return
 	// 	}
@@ -299,7 +299,7 @@ func chairGetNotification(w http.ResponseWriter, r *http.Request) {
 			},
 			Status: status,
 		},
-		RetryAfterMs: 1000,
+		RetryAfterMs: 500,
 	})
 }
 
